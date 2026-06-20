4e04e1435e7c746755ddf8738a4d66843b8d5df3
anhtrung7219 <natrung2510@clc.fitus.edu.vn>
2026-06-20 14:46:40 +0700

    Update the main flow of code for Point2D

--- Lab01_OOP/Lab01_OOP/ex2.cpp
+++ Lab01_OOP/Lab01_OOP/ex2.cpp
@@ -10,13 +10,11 @@ int main()
     SetConsoleOutputCP(CP_UTF8);
     SetConsoleCP(CP_UTF8);
 
-    Point2D A;
+    Point2D A{};
     Point2D B;
 
-    cout << "Mời nhập điểm A:";
-    A.inputPoint();
     cout << "Mời nhập điểm B:";
-    B.inputPoint();
+    cin >> B;
 
     cout << "Khoảng cách giữa 2 điểm A và B là: " << A.calculateDistance(B);
     return 0;
