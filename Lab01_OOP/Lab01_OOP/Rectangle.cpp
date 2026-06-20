2dd91369c7abc55716d70b5dd90c0b92278743fb
anhtrung7219 <natrung2510@clc.fitus.edu.vn>
2026-06-20 18:25:52 +0700

    Update with constructors, destructor, and input/output operator overloading

--- Lab01_OOP/Lab01_OOP/Rectangle.cpp
+++ Lab01_OOP/Lab01_OOP/Rectangle.cpp
@@ -4,36 +4,36 @@
 #include "Rectangle.h"
 using namespace std;
 
-void Rectangle2D::inputRectangle()
-{
-    cout << "Mời nhập đỉnh trái-trên (A):\n";
-    TL.inputPoint();
+// void Rectangle2D::inputRectangle()
+// {
+//     cout << "Mời nhập đỉnh trái-trên (A):\n";
+//     TL.inputPoint();
 
-    cout << "Mời nhập đỉnh phải-trên (B):\n";
-    TR.inputPoint();
+//     cout << "Mời nhập đỉnh phải-trên (B):\n";
+//     TR.inputPoint();
 
-    cout << "Mời nhập đỉnh trái-dưới (C):\n"; // Hoặc trái-dưới tùy theo thứ tự bạn muốn nhập
-    BL.inputPoint();
+//     cout << "Mời nhập đỉnh trái-dưới (C):\n"; // Hoặc trái-dưới tùy theo thứ tự bạn muốn nhập
+//     BL.inputPoint();
 
-    cout << "Mời nhập đỉnh phải-dưới (D):\n";
-    BR.inputPoint();
-}
+//     cout << "Mời nhập đỉnh phải-dưới (D):\n";
+//     BR.inputPoint();
+// }
 
-void Rectangle2D::outputRectangle()
-{
-    cout << "Đỉnh trái-trên A: ";
-    TL.outputPoint();
+// void Rectangle2D::outputRectangle()
+// {
+//     cout << "Đỉnh trái-trên A: ";
+//     TL.outputPoint();
 
-    cout << "Đỉnh phải-trên B: ";
-    TR.outputPoint();
+//     cout << "Đỉnh phải-trên B: ";
+//     TR.outputPoint();
 
-    cout << "Đỉnh trái-dưới C: ";
-    BL.outputPoint();
+//     cout << "Đỉnh trái-dưới C: ";
+//     BL.outputPoint();
 
-    cout << "Đỉnh phải-dưới D: ";
-    BR.outputPoint();
-}
-bool isRightAngle(Point2D A, Point2D B, Point2D C)
+//     cout << "Đỉnh phải-dưới D: ";
+//     BR.outputPoint();
+// }
+static bool isRightAngle(const Point2D &A, const Point2D &B, const Point2D &C)
 {
     double BA_x = A.getX() - B.getX();
     double BA_y = A.getY() - B.getY();
@@ -43,11 +43,13 @@ bool isRightAngle(Point2D A, Point2D B, Point2D C)
 
     double dotProduct = (BA_x * BC_x) + (BA_y * BC_y);
 
-    return abs(dotProduct) < 1e-6;
+    return abs(dotProduct) <= 1e-6;
 }
 
-bool Rectangle2D::isValid()
+bool Rectangle2D::isValid() const
 {
+    if (TL == TR || TL == BL)
+        return false;
     bool TL_angle = isRightAngle(BL, TL, TR);
     bool TR_angle = isRightAngle(TL, TR, BR);
     bool BL_angle = isRightAngle(TL, BL, BR);
@@ -55,12 +57,29 @@ bool Rectangle2D::isValid()
     return TL_angle && TR_angle && BL_angle;
 }
 
-double Rectangle2D::calculatePerimeter()
+double Rectangle2D::calculatePerimeter() const
 {
     return 2 * (TL.calculateDistance(TR) + TL.calculateDistance(BL));
 }
 
-double Rectangle2D::calculateArea()
+double Rectangle2D::calculateArea() const
 {
     return TL.calculateDistance(TR) * TL.calculateDistance(BL);
+}
+istream &operator>>(istream &inDevice, Rectangle2D &R)
+{
+    inDevice >> R.TL >> R.TR >> R.BL >> R.BR;
+    inDevice.ignore();
+    return inDevice;
+}
+ostream &operator<<(ostream &outDevice, const Rectangle2D &R)
+{
+    outDevice << "Đỉnh trái-trên A: " << R.TL;
+
+    outDevice << "Đỉnh phải-trên B: " << R.TR;
+
+    outDevice << "Đỉnh trái-dưới C: " << R.BL;
+
+    outDevice << "Đỉnh phải-dưới D: " << R.BR;
+    return outDevice;
 }
--- Lab01_OOP/Lab01_OOP/Rectangle.h
+++ Lab01_OOP/Lab01_OOP/Rectangle.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "Point.h"
-
+#include <iostream>
+using namespace std;
 class Rectangle2D
 {
 
@@ -11,9 +12,19 @@ private:
     Point2D BR;
 
 public:
-    void inputRectangle();
-    void outputRectangle();
-    bool isValid();
-    double calculatePerimeter();
-    double calculateArea();
+    // CONSTRUCTORS
+    Rectangle2D(const Point2D &A, const Point2D &B, const Point2D &C, const Point2D &D) : TL(A), TR(B), BL(C), BR(D) {}
+    Rectangle2D(double length, double width) : Rectangle2D({-abs(length) / 2, abs(width) / 2}, {abs(length) / 2, abs(width) / 2}, {-abs(length) / 2, -abs(width) / 2}, {abs(length) / 2, -abs(width) / 2}) {}
+    Rectangle2D(double edge) : Rectangle2D(edge, edge) {}
+    Rectangle2D() : Rectangle2D(1.0) {}
+    // void inputRectangle();
+    // void outputRectangle();
+    bool isValid() const;
+    double calculatePerimeter() const;
+    double calculateArea() const;
+    friend istream &operator>>(istream &inDevice, Rectangle2D &a);
+    friend ostream &operator<<(ostream &outDevice, const Rectangle2D &a);
+
+    // DESTRUCTORS
+    ~Rectangle2D() {}
 };
