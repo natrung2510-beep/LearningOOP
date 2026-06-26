3f6a73178e99d30eedd1763157c74d274e7eef72
anhtrung7219 <natrung2510@clc.fitus.edu.vn>
2026-06-26 15:33:11 +0700

    feat(inventory) : add the constructor and getSlot declarations

---
+++ minecraft-inventory-manager/Inventory.h
@@ -0,0 +1,27 @@
+#include "Item.h"
+
+class Inventory
+{
+protected:
+    Item **slot;
+    int capacity;
+    int count;
+
+    void quickSort(int low, int high);
+
+public:
+    // CONSTRUCTORS
+    Inventory(const Inventory &other);
+    Inventory(int capacity);
+    Inventory();
+    // OPERATIONS
+    Item *addItem(Item *item);
+
+    void sortInventory();
+
+    Item **getSlot() const;
+    // OPERATORS
+    Inventory &operator=(const Inventory &other);
+    // DESTRUCTORS
+    ~Inventory();
+};
