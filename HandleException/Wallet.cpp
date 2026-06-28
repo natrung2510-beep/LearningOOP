951c8bdeaa67668d8ff3a91723d531c69d1c4690
anhtrung7219 <natrung2510@clc.fitus.edu.vn>
2026-06-28 16:42:43 +0700

    feat(wallet) : add constructors and destructor implementation with cleanup memory logic

--- HandleException/Wallet.cpp
+++ HandleException/Wallet.cpp
@@ -10,10 +10,26 @@ TransactionException::TransactionException(const char *msg) : runtime_error(msg)
 TransactionException::TransactionException(const char *msg) : runtime_error(msg) {}
 
 // WALLET CLASS IMPLEMENTATIONS
-Wallet::Wallet(const char *name, double balance) {}
-double Wallet::getBalance() const noexcept {}
+
+// Construtors
+Wallet::Wallet(const char *name, double balance)
+{
+    if (balance < 0)
+        throw invalid_argument("Số dư không thể là số âm!");
+    this->balance = balance;
+    this->ownerName = new char[strlen(name) + 1];
+    strcpy(ownerName, name);
+}
+// Operations
+double Wallet::getBalance() const noexcept
+{
+}
 void Wallet::deposit(double amount) {}
 void Wallet::withdraw(double amount) {}
+
+// Destructors
 Wallet::~Wallet()
 {
+    delete this->ownerName;
+    this->ownerName = nullptr;
 }
