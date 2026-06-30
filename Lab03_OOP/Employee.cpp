75d71c572f1201e01f89dadd76192c963c06dff9
anhtrung7219 <natrung2510@clc.fitus.edu.vn>
2026-06-30 21:31:04 +0700

    feat(employee) : implement checking valid information logic in helper functions

--- Lab03_OOP/Employee.cpp
+++ Lab03_OOP/Employee.cpp
@@ -4,7 +4,63 @@
 using namespace std;
 
 // Helper function
+bool Employee::checkValidJobTitle() const
+{
+    if (jobTitle != positions[0] && jobTitle != positions[1] && jobTitle != positions[2] && jobTitle != positions[3])
+        return false;
+    if (workDays < 0)
+        return false;
+    if (workDays <= terms[0] && jobTitle != positions[0])
+        return false;
+    if (workDays > terms[0] && workDays <= terms[1] && jobTitle != positions[1])
+        return false;
+    if (workDays > terms[1] && workDays <= terms[2] && jobTitle != positions[2])
+        return false;
+    if (workDays > terms[2] && jobTitle != positions[3])
+        return false;
 
+    return true;
+}
+bool Employee::checkValidSalaryCoefficient() const
+{
+    if (salaryCoefficient < 0)
+        return false;
+    for (int i = 0; i < 4; i++)
+    {
+        if (jobTitle == positions[i] && salaryCoefficient != salaryMultipliers[i])
+            return false;
+    }
+    return true;
+}
+void Employee::validateInformation()
+{
+    if (workDays < 0)
+        workDays = 0;
+
+    if (!checkValidJobTitle())
+    {
+        if (workDays <= terms[0])
+            jobTitle = positions[0];
+        else if (workDays <= terms[1])
+            jobTitle = positions[1];
+        else if (workDays <= terms[2])
+            jobTitle = positions[2];
+        else
+            jobTitle = positions[3];
+    }
+
+    int target = 0;
+    for (int i = 0; i < 4; i++)
+    {
+        if (jobTitle == positions[i])
+        {
+            target = i;
+            break;
+        }
+    }
+    if (!checkValidSalaryCoefficient())
+        salaryCoefficient = salaryMultipliers[target];
+}
 // CONSTRUCTORS
 Employee::Employee(const std::string &fullName, int workDays, const std::string &jobTitle, const std::string &workName, float salaryCoefficient) {}
 Employee::Employee(const std::string &fullName, int workDays) : Employee(fullName, workDays) {}
