374a08cda39c7e6fd1665cc49a1db084408afab6
anhtrung7219 <natrung2510@clc.fitus.edu.vn>
2026-07-07 21:08:33 +0700

    feat(fraction) : declare getters and setters for Fraction

--- Lab04_OOP/Fraction.h
+++ Lab04_OOP/Fraction.h
@@ -13,8 +13,15 @@ public:
     Fraction(int num);
 
     // OPERATIONS
+    // Calculation tools
     Fraction plus(const Fraction &other) const;
     Fraction substract(const Fraction &other) const;
     Fraction multiply(const Fraction &other) const;
     Fraction divide(const Fraction &other) const;
+    // getter
+    int getNum() const noexcept;
+    int getDenom() const noexcept;
+    // setter
+    void setNum();
+    void setDenom();
 };
