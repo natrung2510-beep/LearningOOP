6c22c2611eb9f50dfa676157374b3ca516a868e6
anhtrung7219 <natrung2510@clc.fitus.edu.vn>
2026-07-09 16:36:43 +0700

    feat(hero) declare constructors for Hero

--- Lab04_OOP/Hero.h
+++ Lab04_OOP/Hero.h
@@ -8,6 +8,23 @@ private:
     std::string heroName;
     unsigned int heroHealth;
     unsigned int heroMana;
-    unsigned int heroLevel;
+    unsigned int heroLevel = 1;
     std::vector<Skill *> skillList;
+
+    static constexpr char *heroName_default = "Valhein";
+    static constexpr unsigned int hp_default = 3000;
+    static constexpr unsigned int mana_default = 500;
+
+public:
+    // CONSTRUCTORs
+    Hero(const std::string &name, unsigned int hp, unsigned int mana, unsigned int level);
+    Hero(const std::string &name, unsigned int hp, unsigned int mana);
+    Hero(const std::string &name, unsigned int level);
+    Hero(const std::string &name);
+    Hero();
+    Hero(const Hero &other);
+
+    // OPERATIONs
+
+    // DESTRUCTOR
 };
