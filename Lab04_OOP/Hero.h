89499fb777bdd500bcc4e014792d482b05df5530
anhtrung7219 <natrung2510@clc.fitus.edu.vn>
2026-07-09 22:44:55 +0700

    refactor(hero) : adjust constructors list

--- Lab04_OOP/Hero.h
+++ Lab04_OOP/Hero.h
@@ -11,20 +11,25 @@ private:
     unsigned int heroLevel = 1;
     std::vector<Skill *> skillList;
 
-    static constexpr char *heroName_default = "Valhein";
+    static constexpr const char *heroName_default = "Valhein";
     static constexpr unsigned int hp_default = 3000;
     static constexpr unsigned int mana_default = 500;
 
 public:
     // CONSTRUCTORs
-    Hero(const std::string &name, unsigned int hp, unsigned int mana, unsigned int level);
+    Hero(const std::string &name, unsigned int hp, unsigned int mana, unsigned int level, const std::vector<Skill *> &skills = {});
     Hero(const std::string &name, unsigned int hp, unsigned int mana);
-    Hero(const std::string &name, unsigned int level);
-    Hero(const std::string &name);
     Hero();
     Hero(const Hero &other);
 
     // OPERATIONs
+    // getter
+
+    // input
+
+    // output
+
+    // others
 
     // DESTRUCTOR
 };
