e3f07dae710d10598981b7a68cac477238a0e569
anhtrung7219 <natrung2510@clc.fitus.edu.vn>
2026-07-10 12:25:29 +0700

    feat(skill) : add constructors implementations for Skill

---
+++ Lab04_OOP/Skill.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+#include "Skill.h"
+#include <stdexcept>
+#include <numeric>
+using namespace std;
+// CONSTRUCTORS
+Skill::Skill(const std::string &name, unsigned int level)
+{
+    if (name.empty())
+        throw invalid_argument("Tên kĩ năng không thể là một chuỗi rỗng!\n");
+    skillName = name;
+    skillLevel = level;
+}
+Skill::Skill(const std::string &name) : Skill(name, 1) {}
+Skill::Skill() : Skill("Tốc biến", 1) {}
+Skill::Skill(const Skill &skill) : Skill(skill.skillName, skill.skillLevel) {}
+// OPERATIONS
+// getter
+std::string Skill::getSkillName() const noexcept {}
+unsigned int Skill::getSkillLevel() const noexcept {}
+// setter
+void Skill::setkillName(const std::string newName) {}
+void Skill::setSkillLevel(unsigned int newLevel) {}
+// input
+void Skill::inputSkill() {}
+// ouput
+void Skill::outputSkill() const noexcept {}
+// DESTRUCTOR
+Skill::~Skill() {}
