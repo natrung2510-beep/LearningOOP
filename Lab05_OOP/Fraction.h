be469e3fe03e62be1052bf297f06812921727e5f
anhtrung7219 <natrung2510@clc.fitus.edu.vn>
2026-07-16 12:32:12 +0700

    feat(fraction): add header file with all needed declarations

---
+++ Lab05_OOP/Fraction.h
@@ -0,0 +1,54 @@
+#pragma once
+#include <iostream>
+class Fraction
+{
+private:
+    int *numerator;
+    int *denominator;
+
+public:
+    // CONSTRUCTORs
+    Fraction(int num, int denom);
+    Fraction(int num);
+    Fraction();
+    Fraction(const Fraction &other);
+
+    // OPERATORs
+    // assignment
+    Fraction &operator=(const Fraction &other);
+
+    // arithmetic
+    Fraction operator+(const Fraction &other) const;
+    Fraction operator-(const Fraction &other) const;
+    Fraction operator*(const Fraction &other) const;
+    Fraction operator/(const Fraction &other) const;
+
+    // comparison
+    bool operator<(const Fraction &other) const;
+    bool operator>(const Fraction &other) const;
+    bool operator<=(const Fraction &other) const;
+    bool operator>=(const Fraction &other) const;
+    bool operator==(const Fraction &other) const;
+    bool operator!=(const Fraction &other) const;
+
+    // arithmetic assignment
+    Fraction &operator+=(const Fraction &other);
+    Fraction &operator-=(const Fraction &other);
+    Fraction &operator*=(const Fraction &other);
+    Fraction &operator/=(const Fraction &other);
+
+    // increment
+    Fraction operator++();
+    Fraction &operator++(int);
+    // decrement
+    Fraction operator--();
+    Fraction &operator--(int);
+
+    // input
+    friend std::istream operator>>(std::istream &in, Fraction &f);
+    // output
+    friend std::ostream operator<<(std::ostream &out, const Fraction &f) noexcept;
+
+    // DESTRUCTOR
+    ~Fraction();
+};
