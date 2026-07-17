7638751e3c69fc7c373d1fdd974621171d987f94
anhtrung7219 <natrung2510@clc.fitus.edu.vn>
2026-07-17 23:27:01 +0700

    feat(myIntArray): add cpp file with the skeleton of code

---
+++ Lab05_OOP/myIntArray.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+#include "myIntArray.h"
+#include <stdexcept>
+using namespace std;
+
+// CONSTRUCTORs
+MyIntArray::MyIntArray(int size = 0) {}
+MyIntArray::MyIntArray(int size, int default_value) {}
+
+// OPERATORs
+// assigment
+MyIntArray &MyIntArray::operator=(const MyIntArray &other) {}
+// plus
+MyIntArray MyIntArray::operator+(const MyIntArray &other) {}
+// prefix increment
+MyIntArray &MyIntArray::operator++() {}
+// postfix increment
+MyIntArray MyIntArray::operator++(int) {}
+// input
+istream &operator>>(istream &in, MyIntArray &array) {}
+// output
+ostream &operator<<(ostream &out, const MyIntArray &array) {}
