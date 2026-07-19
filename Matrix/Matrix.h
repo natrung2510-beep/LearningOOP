9ba58b3231bc8d32cbeb02bfe5d6beaa5bbce2b5
anhtrung7219 <natrung2510@clc.fitus.edu.vn>
2026-07-19 23:03:53 +0700

    feat(matrix): add header file with all neccessary declarations

--- Matrix/Matrix.h
+++ Matrix/Matrix.h
@@ -9,4 +9,20 @@ protected:
     int **data;
 
 public:
+    // Constructors
+    Matrix(int rows = 0, int cols = 0, int default_value = 0);
+    Matrix(const Matrix &other);
+
+    // OPERATORs
+    // assignment
+    Matrix &operator=(const Matrix &other);
+    Matrix operator+(const Matrix &other) const;
+    Matrix operator*(const Matrix &other) const;
+    int &operator()(int row_idx, int col_idx) noexcept;
+    int operator()(int row_idx, int col_idx) const noexcept;
+    friend std::istream &operator>>(std::istream &in, Matrix &mat);
+    friend std::ostream &operator<<(std::ostream &out, const Matrix &mat);
+
+    // DESTRUCTOR
+    ~Matrix();
 };
